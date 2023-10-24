bb670eeb606026825b3aa2b94b02720a1ead04e6
VZolotonosha <zolotonosha.itsmart@gmail.com>
2023-10-24 13:23:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,22 +12,22 @@ bot = telebot.TeleBot(API_TOKEN)
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.add("Включить")
-    markup.add("Выключить")
-    markup.add("Получить скриншот")
-    bot.send_message(message.chat.id, 'Приветсвую', reply_markup=markup)
+    markup.add("turnOn")
+    markup.add("turnOff")
+    markup.add("Get Screenshot")
+    bot.send_message(message.chat.id, 'Welcome', reply_markup=markup)
 
-@bot.message_handler(regexp='включить')
+@bot.message_handler(regexp='turnOn')
 def turn_on_computer():
-    mac_address = 'FC:E2:6C:00:57:C7'
+    mac_address = 'YOUR_MAC_ADDRESS'
     subprocess.call(['wakeonlan', mac_address])
 
-@bot.message_handler(regexp='выключить')
+@bot.message_handler(regexp='turnOff')
 def echo_message(message):
     bot.send_message(message.chat.id, 'Выключаю...')
     os.system("sudo shutdown -h 0")
 
-@bot.message_handler(regexp='получить скриншот')
+@bot.message_handler(regexp='Get Screenshot')
 def echo_message(message):
     path = tempfile.gettempdir() + 'screenshot.png'
     screenshot = ImageGrab.grab()
